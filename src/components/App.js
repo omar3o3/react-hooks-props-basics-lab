162aafd8dab2b86e2d3feac4a9b08a1eb4d311ec
omar3o3 <otantawy53@gmail.com>
2022-05-17 15:16:58 -0400

    commit tree ebc80f19c6c0ac0011794678b7c39bed073c8540

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,20 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+
+// pass this data down as props to the child component(s) that need it!
+
+// console.log(user);
+// console.log(user.links)
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color = {user.color} name = {user.name} city = {user.city}/>
+      <About bio = {user.bio} links = {user.links}/>
+
     </div>
   );
 }
